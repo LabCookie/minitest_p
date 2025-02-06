856d57f8adc81c3eca5810d751956788b8ff27fa
LabCookie <golf.le.fleur26@outlook.com>
2025-02-06 07:29:45 -0600

    Update init.lua

--- mods/mini_crafting/init.lua
+++ mods/mini_crafting/init.lua
@@ -57,7 +57,7 @@ core.register_craft({
 })
 
 core.register_craft({
-	output = "mini_nodes:gold_apple",
+	output = "mini_items:gold_apple",
 	recipe = {
 		{"mini_items:gold_bar","mini_items:gold_bar","mini_items:gold_bar"},
 		{"mini_items:gold_bar","mini_items:apple","mini_items:gold_bar"},
